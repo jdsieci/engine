37087c72e44f57b3fb2da863e8031c7844128391
Jerzy Drozdz <jerzy.drozdz@jdsieci.pl>
2011-09-19 21:10:17 +0200

    Testy submodulow

--- setup.py
+++ setup.py
@@ -5,3 +5,6 @@ try:
   from setuptools import setup
 except ImportError:
   from distutils.core import setup
+
+def read(fname):
+  return open(os.path.join(os.path.dirname(__file__), fname)).read()
